4bbb513e66cb58f7e50b41638ac558cee16befce
Doberlius <tbuaekem@gmail.com>
2025-07-06 22:04:37 +0700

    make some changes

--- SWpointshop/src/services/api.js
+++ SWpointshop/src/services/api.js
@@ -6,6 +6,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  timeout: 10000, // 10 second timeout
 });
 
 // Add a request interceptor to include the auth token
@@ -15,6 +16,30 @@ api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
+}, (error) => {
+  console.error('Request error:', error);
+  return Promise.reject(error);
+});
+
+// Add a response interceptor to handle errors
+api.interceptors.response.use((response) => {
+  return response;
+}, (error) => {
+  if (error.code === 'ERR_NETWORK') {
+    console.error('Network error - please check your connection and try again');
+  } else if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.error('Response error:', error.response.data);
+    console.error('Status:', error.response.status);
+  } else if (error.request) {
+    // The request was made but no response was received
+    console.error('No response received:', error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.error('Error:', error.message);
+  }
+  return Promise.reject(error);
 });
 
 export default api; 
